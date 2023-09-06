569505a8d0a2c1deef7db81f25e4a4bbb778d134
Sardor <sardormahmudov16@gmail.com>
2023-09-06 17:20:51 +0500

    bugs related to base_url fixed

--- admin/src/utils/requestMethods.js
+++ admin/src/utils/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.API_URL;
+const BASE_URL = import.meta.env.MODE === 'development' ? import.meta.env.VITE_API_URL : import.meta.env.API_URL;
 const currentUser = JSON.parse(localStorage.getItem('currentUser'));
 
 const token = currentUser?.accessToken
